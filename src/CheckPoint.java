0c1c89ba06e6fe8d6f66aee1a0452b15783f211d
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-05-17 12:40:11 -0700

    Checkpoint 0

---
+++ src/CheckPoint.java
@@ -0,0 +1,13 @@
+import java.awt.Color;
+
+import javax.swing.JOptionPane;
+
+public class CheckPoint { public static void main(String[] args) {
+	String color = JOptionPane.showInputDialog("What's your favorite color ?");
+	JOptionPane.showMessageDialog(null, color + "Oh really that's my favorite color too");
+	Robot r2d2 = new Robot();
+	
+}
+	
+}
+}
